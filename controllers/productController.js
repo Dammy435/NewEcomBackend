363fff7822ca3aa58efb0d38c23ed14ecd7fa5b0
Dammy435 <damilolaajulo@yahoo.com>
2025-07-14 10:22:16 +0100

    updated product routes

--- controllers/productController.js
+++ controllers/productController.js
@@ -27,3 +27,28 @@ exports.createProduct = async (req, res) => {
     const createdProduct = await product.save();
     res.status(201).json(createdProduct);
 };
+//  Later: Implement update and delete functionality with proper admin checks
+exports.updateProduct = async (req, res) => {
+    const { name, brand, description, price, countInStock, image, category } = req.body;
+
+    const product = await Product.findById(req.params.id);
+    if (!product) return res.status(404).json({ message: "Product not found" });
+
+    product.name = name || product.name;
+    product.brand = brand || product.brand;
+    product.description = description || product.description;
+    product.price = price || product.price;
+    product.countInStock = countInStock || product.countInStock;
+    product.image = image || product.image;
+    product.category = category || product.category;
+
+    const updatedProduct = await product.save();
+    res.json(updatedProduct);
+}
+exports.deleteProduct = async (req, res) => {
+    const product = await Product.findById(req.params.id);
+    if (!product) return res.status(404).json({ message: "Product not found" });
+
+    await product.remove();
+    res.json({ message: "Product removed" });
+}
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -2,7 +2,10 @@ const express = require("express");
 const {
     getAllProducts,
     getProductById,
-    createProduct
+    createProduct,
+    deleteProduct,
+    updateProduct
+
 } = require("../controllers/productController");
 
 const router = express.Router();
@@ -10,5 +13,7 @@ const router = express.Router();
 router.get("/", getAllProducts);
 router.get("/:id", getProductById);
 router.post("/", createProduct); // Later: Protect this for admin use
+router.put("/:id", updateProduct); // Later: Protect this for admin use
+router.delete("/:id", deleteProduct); // Later: Protect this for admin use
 
 module.exports = router;
